e2afafbd9a6adc7d240d00bf083e9c4b93fcb21a
tony <iam@antoniovargas.io>
2019-01-30 16:05:42 -0600

    Worked on the navigation bar

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import * as ROUTES from "../../constants/routes";
+import Navigation from "../Navigation";
+import LandingPage from "../Landing";
+import SignInPage from "../SignIn";
+import SignUpPage from "../SignUp";
+import HomePage from "../Home";
+import AdminPage from "../Admin";
+import PasswordForgetPage from "../PasswordForget";
+import AccountPage from "../Account";
 
-function App() {
-  return (
-    <div>
-      <h1>App</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navigation />
+          <hr />
+          <Route exact path={ROUTES.LANDING} component={LandingPage} />
+          <Route exact path={ROUTES.HOME} component={HomePage} />
+          <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
+          <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
+          <Route exact path={ROUTES.ADMIN} component={AdminPage} />
+          <Route exact path={ROUTES.ACCOUNT} component={AccountPage} />
+          <Route
+            exact
+            path={ROUTES.PASSWORD_FORGET}
+            component={PasswordForgetPage}
+          />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,9 +1,18 @@
 import React from "react";
+import "./Navigation.scss";
+import { Link } from "react-router-dom";
+import * as ROUTES from "../../constants/routes";
 
 function Navigation() {
   return (
-    <div>
-      <h1>Navigation</h1>
+    <div className="Navigation">
+      <ul>
+        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+        <Link to={ROUTES.LANDING}>Landing</Link>
+        <Link to={ROUTES.HOME}>Home</Link>
+        <Link to={ROUTES.ACCOUNT}>Account</Link>
+        <Link to={ROUTES.ADMIN}>Admin</Link>
+      </ul>
     </div>
   );
 }
